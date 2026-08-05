06e490fb0230fc415781920a4138f7b0b2f058a1
marcusaristonkikerpill <151571662+KiksOfficial@users.noreply.github.com>
2026-08-05 20:52:12 +0300

    later will add realloc for cases if len of line > 1024 bytes

--- grep.c
+++ grep.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
 
-  char buffer[1024];
+  size_t capacity = 1024;
+  char *buffer = malloc(capacity);
 
   if (argc < 3) {
     perror("grep-btw");
+    free(buffer);
+    buffer = NULL;
     return 1;
   }
-  FILE *fptr = fopen(argv[1], "r");
+  FILE *fptr = fopen(argv[2], "r");
   if (fptr == NULL) {
+    free(buffer);
+    buffer = NULL;
     return 1;
   }
 
-  while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
-    if (strstr(buffer, argv[2])) {
+  while (fgets(buffer, capacity, fptr) != NULL) {
+    if (strstr(buffer, argv[1])) {
       printf("%s", buffer);
     }
   }
 
   fclose(fptr);
+  free(buffer);
+  buffer = NULL;
   return 0;
 }
